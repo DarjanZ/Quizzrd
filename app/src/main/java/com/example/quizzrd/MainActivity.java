10f50845bf09e68eed3b0c38ab2edc08e24c5746
DarjanZ <darjanus789@gmail.com>
2021-01-15 01:18:40 +0100

    Commit.

--- app/src/main/java/com/example/quizzrd/MainActivity.java
+++ app/src/main/java/com/example/quizzrd/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinnerDifficulty;
     private int highscore;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
